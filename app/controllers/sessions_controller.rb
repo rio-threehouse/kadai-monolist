1f631c7d1c8b5a55a01c53cb37bf6126b0c359fb
rio.threehouse <threehouse.second@gmail.com>
2019-03-17 03:38:21 +0000

    want

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,7 +17,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    flash[:succerr] = 'ログアウトしました'
+    flash[:success] = 'ログアウトしました'
     redirect_to root_url
   end
   
--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
-  resources :items, only: [:new]
+  resources :items, only: [:show, :new]
+  resources :ownerships, only: [:create, :destroy]
 end
